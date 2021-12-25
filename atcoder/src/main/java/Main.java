207107357f3e7f8b7ba5546b2f8a926cb2880d4a
Hayato Takahashi <hayatostar1025@gmail.com>
2021-12-25 16:09:16 +0900

    Mainクラスのデフォルトを作成

--- atcoder/src/main/java/Main.java
+++ atcoder/src/main/java/Main.java
@@ -1,9 +1,24 @@
-/**
- * Hello world!
- *
- */
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+	public static void main(String args[]) {
+
+		// 入力
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int n = Integer.parseInt(sc.next());
+		Integer[] a = new Integer[n];
+		Arrays.setAll(a, i -> Integer.parseInt(sc.next()));
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(sc.next());
+		}
+		sc.close();
+
+		// 主処理
+		int result = 0;
+
+		// 出力
+		System.out.println(result);
+	}
 }
